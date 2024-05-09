6138051453986814a30bb5af59f06ee8e147b161
Anish Tiwari <anish.at.tiwari@gmail.com>
2024-05-09 14:30:05 +0530

    chore: build fixes

--- src/app/feed/mainComponent.tsx
+++ src/app/feed/mainComponent.tsx
@@ -8,8 +8,10 @@ const MainComponent = () => {
     <div className='border-l-2 w-full  text-white border-l-[#433F48]'>
       <Header />
       <div className=' flex flex-wrap gap-9 items-center  justify-centent py-12 px-8'>
-        {[0, 1, 2, 3, 4, 5, 6, 7].map(() => (
-          <ModelCard />
+        {[0, 1, 2, 3, 4, 5, 6, 7].map((index) => (
+          <React.Fragment key={index}>
+            <ModelCard />
+          </React.Fragment>
         ))}
       </div>
     </div>
