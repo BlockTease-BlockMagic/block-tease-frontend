e908fe4a80724b1169dd47cf64da002e056d74ad
Anish Tiwari <anish.at.tiwari@gmail.com>
2024-05-09 21:28:11 +0530

    feat: added polygon id logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
       <div className='absolute top-0  right-0 w-1/2 h-full p-6 '>
         <div className=' flex flex-col items-center h-full justify-around'>
           <Image src={logo} priority alt='logo' width={300} height={200} />
-          <div className='flex flex-col items-center justify-between  gap-10'>
+          <div className='flex flex-col items-center justify-between  gap-10 relative'>
             <p className='text-2xl font-medium bg-[rgb(148,60,255)] bg-gradient-to-r from-[#943cff] to-[rgba(251,3,147,1)] bg-clip-text text-transparent text-center'>
               "Empower your content, <br />
               redefine connection."
@@ -31,14 +31,24 @@ export default function HomePage() {
             </p>
             <div className='flex flex-col items-center gap-1'>
               <Image priority src={qr} alt='logo' height={250} width={250} />
+              <div className='absolute left-[80%] top-1/2 h-[120px] w-[120px] flex items-center'>
+                <Image
+                  src={
+                    'https://assets-global.website-files.com/637359c81e22b715cec245ad/63dc31f8817a4a509d7635a7_Logo.svg'
+                  }
+                  height={300}
+                  width={300}
+                  alt='polygon_logo'
+                />{' '}
+                <div>🆔</div>
+              </div>
+
               <p className='bg-[rgb(255,255,255)]  bg-gradient-to-r from-[rgba(255,255,255,1)] to-[rgba(255,60,212,1)] bg-clip-text text-transparent text-center font-bold text-lg '>
                 Scan to verify 18+
               </p>
               <span>or</span>
-              <Link href="feed">
-
-                <BorderGlowButton buttonFor="Skip for now " />
-
+              <Link href='feed'>
+                <BorderGlowButton buttonFor='Skip for now ' />
               </Link>
             </div>
           </div>
